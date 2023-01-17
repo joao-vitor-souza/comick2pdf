0bc73b2d8af202d343a4127f05a5464b63a1d22c
João Vitor <joaovitorsouza205@hotmail.com>
2023-01-17 17:36:44 -0300

    build: enviroment paths variables

---
+++ env_paths.py
@@ -0,0 +1,13 @@
+"""
+The variable DOWNLOAD_PATH is where the files you're going to download will be stored.
+The variable OUTPUT_PATH is where the PDF file will be stored.
+
+Example:
+
+DOWNLOAD_PATH = "/home/joao/Downloads/"
+OUTPUT_PATH = "/home/joao/Mangas/"
+
+"""
+
+DOWNLOAD_PATH = ""
+OUTPUT_PATH = ""
